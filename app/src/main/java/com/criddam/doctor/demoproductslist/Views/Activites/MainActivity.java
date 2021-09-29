6895ab214fac1d6687fedafdbf2486913a2a2995
Mind Game <md.izajul@gmail.com>
2021-09-29 23:05:45 +0600

    Initial Commit - packages created

---
+++ app/src/main/java/com/criddam/doctor/demoproductslist/Views/Activites/MainActivity.java
@@ -0,0 +1,16 @@
+package com.criddam.doctor.demoproductslist.Views.Activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.criddam.doctor.demoproductslist.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
