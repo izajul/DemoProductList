db978d9e491a7182ec835589afea15361f1279c7
Mind Game <md.izajul@gmail.com>
2021-09-30 04:36:20 +0600

    Product List Design & Shown Done

--- app/src/main/java/com/criddam/doctor/demoproductslist/Views/Activites/MainActivity.java
+++ app/src/main/java/com/criddam/doctor/demoproductslist/Views/Activites/MainActivity.java
@@ -3,6 +3,7 @@ package com.criddam.doctor.demoproductslist.Views.Activites;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
@@ -13,10 +14,14 @@ import com.criddam.doctor.demoproductslist.Dagger.Componets.RetroComponent;
 import com.criddam.doctor.demoproductslist.Models.DataModels.ProductDataModel;
 import com.criddam.doctor.demoproductslist.R;
 import com.criddam.doctor.demoproductslist.ViewModels.ProductViewModel;
+import com.criddam.doctor.demoproductslist.Views.Adapters.ProductsListAdapter;
+import com.criddam.doctor.demoproductslist.databinding.ActivityMainBinding;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import dagger.BindsInstance;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -27,12 +32,18 @@ public class MainActivity extends AppCompatActivity {
 
     private ProductViewModel productViewModel;
 
+    private ActivityMainBinding binding;
+
+    private ProductsListAdapter mAdapter;
+    private List<ProductDataModel> list = new ArrayList();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        productViewModel = new ViewModelProvider(this).get(ProductViewModel.class);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        productViewModel = new ViewModelProvider(this).get(ProductViewModel.class);
 
         ProgressDialog progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Loading...");
@@ -40,8 +51,17 @@ public class MainActivity extends AppCompatActivity {
         progressDialog.show();
         productViewModel.getProductList().observe(this, productDataModels -> {
             progressDialog.dismiss();
-
+            list.clear();
+            list.addAll(productDataModels);
+            mAdapter.notifyDataSetChanged();
 
         });
+
+        mAdapter = new ProductsListAdapter(list);
+
+//        binding.listRC.setLayoutManager(new GridLayoutManager(this,2));
+        binding.listRC.setLayoutManager(new GridLayoutManager(this,2));
+
+        binding.listRC.setAdapter(mAdapter);
     }
 }
---
+++ app/src/main/java/com/criddam/doctor/demoproductslist/Views/Adapters/ProductsListAdapter.java
@@ -0,0 +1,70 @@
+package com.criddam.doctor.demoproductslist.Views.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.criddam.doctor.demoproductslist.Models.DataModels.ProductDataModel;
+import com.criddam.doctor.demoproductslist.R;
+import com.criddam.doctor.demoproductslist.databinding.ProductListRowBinding;
+
+import java.util.List;
+
+public class ProductsListAdapter extends RecyclerView.Adapter<ProductsListAdapter.MyViewHolder> {
+
+    private List<ProductDataModel> list;
+
+    public ProductsListAdapter(List<ProductDataModel> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(ProductListRowBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        ProductDataModel item = list.get(position);
+
+        holder.binding.desc.setText(item.getDescription());
+        holder.binding.shortDesc.setText(item.getShortName());
+
+
+
+        if (item.getFiles()!=null){
+            String fileUuid = item.getFiles().get(0).getFileUuid();
+            Glide
+                    .with(holder.itemView.getContext())
+                    .load("https://labapi.yuma-technology.co.uk:8443/delivery/product/"
+                            +item.getProductUuid()
+                            +"/file/"+fileUuid)
+                    .error(R.drawable.default_img)
+                    .placeholder(R.drawable.image_loader)
+                    .into(holder.binding.img);
+        }
+
+
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        ProductListRowBinding binding;
+        public MyViewHolder(@NonNull ProductListRowBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
